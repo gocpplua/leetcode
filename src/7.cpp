9d664de2055ba00164b2f44c4f5ec54ae8e5a64c
chenqi <chenqi@ct108.com>
2020-11-19 15:34:39 +0800

    7. 整数反转

---
+++ src/7.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<assert.h>
+#include<vector>
+#include <utility>
+#include <unordered_set>
+using namespace std;
+
+// 官方的代码有多余，因为int类型最大数和最小数开头的数字只能是1或2，所以如果有最后一次循环的话，pop的值一定为1或2
+// https://leetcode-cn.com/problems/reverse-integer/solution/zheng-shu-fan-zhuan-by-leetcode/108970
+int reverse(int x) {
+    int recv = 0;
+    while (x != 0)
+    {
+        int temp = x %10;
+        x /= 10;
+        if (recv > INT_MAX / 10 || (recv ==  INT_MAX / 10 && temp > 7))
+        {
+            return 0;
+        }
+        if (recv < INT_MIN / 10 || (recv ==  INT_MIN / 10 && temp < -8))
+        {
+            return 0;
+        }
+        
+        recv = recv * 10 + temp;
+    }
+    return recv;
+}
+int main()
+{
+    return 0;
+}
