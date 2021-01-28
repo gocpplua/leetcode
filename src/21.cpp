6a6a1b4131c62c165a272489a10d6dac52db1a00
chenqi <chenqi@ct108.com>
2021-01-28 11:06:30 +0800

    21. 合并两个有序链表

--- src/21.cpp
+++ src/21.cpp
@@ -1,2 +1,18 @@
 // https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnnbp2/
 // https://leetcode-cn.com/problems/merge-two-sorted-lists/
+
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
+ class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        
+    }
+};
